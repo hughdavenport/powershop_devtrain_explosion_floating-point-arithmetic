07f5fdea23db8702f21210e021454da683422920
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-02-15 12:47:01 +1300

    Remove test for integers

--- spec/myfloat_spec.rb
+++ spec/myfloat_spec.rb
@@ -5,11 +5,7 @@ RSpec.describe MyFloat do
   describe "value" do
     it "should have a value of 0 by default" do
        f = MyFloat.new
-       expect(f.value).to eq 0
-    end
-    it "should allow passing an integer constructor" do
-       f = MyFloat.new 1
-       expect(f.value).to eq 1
+       expect(f.value).to eq 0.0
     end
     it "should represent 0.15625 correctly" do
        f = MyFloat.new 0b00111110001000000000000000000000
